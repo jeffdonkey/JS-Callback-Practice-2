cf21f24a63ddfd8624124797d3796db3ed9d3f0a
jeffdonkey <8jefa8@gmail.com>
2022-08-02 17:21:14 -0400

    task 4 done

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Please refer to the Activity Guide in Canvas for directions.
 jheller notes-
 Part 1- ok
 Part 2- ok
+_____________________________________________________________________________
 
 Part 3- seeing something I did not expect.  In part three we enable the arrow
 keys for character movement.  When I run index.html in the browser it appears
@@ -16,3 +17,20 @@ when keys are depressed.
 
 Did get the issue above fixed.  Susan saw that the function(move) was not being
 invoked in function moveCharacter.
+
+code can be found in: https://github.com/jeffdonkey/JS-Callback-Practice.git
+________________________________________________________________________________
+
+Part 4- copied code from Activity page; no joy; no character movement; no change
+occurring in html file (see Part 3 notes).  
+
+Looked at html file in browser tools.  The img for the character no longer has a
+x and y position.
+
+This issue turned-out to be me.  I misread the Activity text and did not put
+logic in function moveWithArrowKeys.  Instead I had a blank
+function moveWithArrowKeys inside of the function move and moveWithArrowKeys
+as it's own function outside of function move.
+_____________________________________________________________________________
+
+Part 5-
--- index.js
+++ index.js
@@ -1,77 +1,11 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-/*below is added code from the directions.
-purpose of this code is to set-up user inputed movement via
-the arrow keys on the keyboard */
 
-/*line 11 is declaring the value for the character
-The value is created when the "..green-character.." value
-is sent to the function newImage */
 const character = newImage('assets/green-character/static.gif')
-/*lines 13-15 and setting the initial screen placement for character */
-let direction = null;
-let x = 100;
-let y = 250;
+move(character).withArrowKeys(100, 250)
 
-/*function moveCharacter will update x or y based
-on inputs (west, east, north, south).  */
-function moveCharacter(){ 
-    if(direction === 'west'){
-        x = x - 1
-    }
-    if(direction === 'north'){
-        y = y + 1
-    }
-    if(direction === 'east'){
-        x = x + 1
-    }
-    if(direction === 'south'){
-        y = y - 1
-    }
-    /*I am not sure what the two lines below are doing.
-    I think they are passing updated information back to
-    function move; specifically to function moveToCoordinates
-    that is within function move */
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-/* line below was added to invoke function move.
-without this line the x and y values would change
-in the html file but the graphic would not show movement.
-I did not come up with this on my own.  I got help from
-the spouse. */
-    move(character)
-}
 
-/*setInterval is a pre-set JavaScript function.
-the first argument is the target (function moveCharacter).
-The second argument is a number value for the amount
-of milliseconds to run before re-invoking */
-setInterval(moveCharacter, 1)
-
-/* below is an alternate function where the 
-if statements that would change the character's position
-are inbedded within the function setInterval.
-Activity directions expressed that it is our choice on which
-function to use. 
-
-setInterval(function(){ 
-    if(direction === 'west'){
-        x = x - 1
-    }
-    if(direction === 'north'){
-        y = y + 1
-    }
-    if(direction === 'east'){
-        x = x + 1
-    }
-    if(direction === 'south'){
-        y = y - 1
-    }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}, 1)
-*/
 
 
 move(newImage('assets/tree.png')).to(200, 450)
@@ -83,23 +17,3 @@ move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
 
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-    
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
